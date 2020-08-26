61516c97cddb6de39c96e7410adcd4110d7ee0fb
atsukofu <atsukofu0527@gmail.com>
2020-08-26 22:42:56 +0900

    add staff page

---
+++ staff/staff-add-done.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>おいしいあんこ屋さん 箕面製餡所</title>
+  <link rel="stylesheet" href="../styles/index.css">
+  <link rel="stylesheet" href="../styles/reserve.css">
+  <link href="https://use.fontawesome.com/releases/v5.6.1/css/all.css" rel="stylesheet">
+</head>
+<body>
+  <header>
+    <div class="header-left">
+      <p class="header-text">大阪北部の箕面市で100年愛されるあんこやさんです。</p>
+      <img src="../images/logo.png" class="header-logo">
+    </div>
+    <div class="header-right">
+      <div class="header-tel">
+        <i class="fas fa-phone-volume"></i>
+        <span class="telnumber">072-014-085</span>
+        <p class="business-time">月〜金 9:00~18:00</p>
+      </div>
+      <div class="header-menu">
+        <ul class="menu-list">
+          <li><a href="#">会社概要</a></li>
+          <li><a href="product/pro-index.php">商品紹介</a></li>
+          <li><a href="reserve/new-reserve.php">和菓子教室</a></li>
+          <li><a href="contact/contact-new.html">お問い合わせ</a></li>
+          <li>
+            <i class="fab fa-facebook-square"></i>
+            <i class="fab fa-twitter-square"></i>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </header>
+  <section>
+  <h1 class="title">スタッフ登録確認</h1>
+  <div class="customer-form-wrapper" style="padding-left: 300px;">
+  <?php
+
+  try{
+    $staff_name = $_POST['name'];
+    $staff_pass = $_POST['password'];
+    
+    $staff_name = htmlspecialchars($staff_name,ENT_QUOTES,'UTF-8');
+    $staff_pass = htmlspecialchars($staff_pass,ENT_QUOTES,'UTF-8');
+
+    $dsn = 'mysql:dbname=ankoproduct;host=localhost;charset=utf8';
+    $user = 'root';
+    $password = '';
+    $dbh = new PDO($dsn,$user,$password);
+    $dbh -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    $sql = 'INSERT INTO staff (name, password) VALUES(?,?)';
+    $stmt = $dbh->prepare($sql);
+    $data[] = $staff_name;
+    $data[] = $staff_pass;
+    $stmt->execute($data);
+
+    $dbh = null;
+
+    print $staff_name."さんを登録しました。<br/>";
+    
+  } catch(Exception $e) {
+    print 'ただいま障害により大変ご迷惑をおかけしております。';
+    exit();
+  }
+?>
+<br/>
+<a href="staff-add.html" class="backlink">スタッフ登録ページへ戻る</a>
+</section>
+  <div class="footer">
+    <div class="footer-top">
+      <ul class="footer-list">
+        <li><a href="/minohseian/index.html">HOME</a></li>
+        <li><a href="#">会社概要</a></li>
+        <li><a href="/minohseian/product/pro-index.php">商品紹介</a></li>
+        <li><a href="/minohseian/reserve/new-reserve.php">和菓子教室</a></li>
+        <li><a href="/minohseian/contact/contact-new.html">お問い合わせ</a></li>
+        <li><a href="#">STAFF</a></li>
+      </ul>
+      <div class="footer-company-wrapper">
+      <img src="../images/footer_logo.png" class="footer-logo">
+        <div class="footer-address">〒590-0021<br/>大阪府箕面市みのお台<br/>1丁目23番</div>
+      </div>
+      </div>
+    <div class="footer-bottom">
+      Copyright (c) 箕面製饀所 Co., Ltd All rights reserved.
+    </div>
+  </div>
+</body>
+</html>
